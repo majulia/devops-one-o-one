d2b3424fb44e650b9267979e52e1b3c009af15f9
Maria Julia de Oliveira <52303035+majulia@users.noreply.github.com>
2022-07-06 20:00:36 -0300

    updated UnitTest

--- cicdTest/UnitTest1.cs
+++ cicdTest/UnitTest1.cs
@@ -9,13 +9,23 @@ namespace cicdTest
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(Helper.GetDescription(-1), "COLD");
+            Assert.AreEqual(Helper.GetDescription(0), "COLD");
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            Assert.AreEqual(Helper.GetDescriptionToLower(-1), "cold");
+            Assert.AreEqual(Helper.GetDescriptionToLower(0), "cold");
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(Helper.GetDescription(-1), "FREEZING");
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(Helper.GetDescriptionToLower(-1), "freezing");
         }
     }
 }
